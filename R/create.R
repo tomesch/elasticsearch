10a7eb7c13b6d0486058c39c349ff16a7d2812f5
Thomas Eschemann <tom@eschemann.fr>
2014-12-09 00:21:08 +0100

    add create function

---
+++ R/create.R
@@ -0,0 +1,10 @@
+#' @export
+Create <- function (index, type, id, document, version = NULL,
+                    version_type = NULL, routing = NULL,
+                    parent = NULL, timestamp = NULL, ttl = NULL, refresh = FALSE,
+                    timeout = "1m", raw = FALSE, validate.params = TRUE) {
+  args = as.list(match.call())
+  args[[1]] = NULL
+  args[['op_type']] = 'create'
+  do.call(Index, args)
+}

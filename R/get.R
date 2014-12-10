ab8c88cd25646d47a85cced5ed33547b39704f56
claudiat <tanaseclaudiag@gmail.com>
2014-12-10 10:38:35 +0100

    add documentation for new parameters

--- R/get.R
+++ R/get.R
@@ -2,11 +2,22 @@
 #'
 #' \code{get} searches a document in an index based on its id and returns it.
 #'
-#' @param index A string representing the index
-#' @param type A string representing the type
-#' @param id A string representing the id
-#' @param field A string representing the field
-#' @param raw A string representing the format
+#' @param index A string representing the index.
+#' @param type A string representing the type.
+#' @param id A string representing the id.
+#' @param fields A string representing the fields.
+#' @param source A boolean representing our choice to return the contents of the _source field or not.
+#' @param source_include A string representing the source_include; include parameters that you want to display in the returned source.  
+#' @param source_exclude A string representing the source_exclude; filter parameters that you do not want to display in the returned source.
+#' @param realtime A boolean representing if the result should be realtime or not/
+#' @param routing A string allowing to control the _routing aspect when indexing data and explicit routing control is required.
+#' @param preference A string that controls a preference of which shard replicas to execute the search request on. 
+#' @param refresh A boolean that allows to explicitly refresh one or more index, making all operations performed since the last refresh available for search. 
+#' @param version A string representing the return of a version for each search hit.
+#' @param exists A boolean that returns documents that have at least one non-null value in the original field.
+#' @param raw A boolean that indicates if the format of the response should be in json or not.
+#' @param validate.params A boolean representing the validate.params
+#' @param get.source A boolean representing the get.source
 #'
 #' @examples
 #' get("twitter", id="1")

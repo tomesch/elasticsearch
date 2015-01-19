ddc1169f5a2cfc8f0c2b1ff98f2475ae50c1d7f5
Joris <joris.girardot@gmail.com>
2015-01-19 17:07:38 +0100

    create default test index before runing unit tests

--- tests/testthat/test_a.R
+++ tests/testthat/test_a.R
@@ -1,5 +1,5 @@
 es = ElasticSearchClient("http://localhost:9200")
 
-if (indices.exists(es, "test_r_elasticsearch")) {
+if (indices.exists(es, "test_r_elasticsearch")$exists) {
   indices.delete(es, "test_r_elasticsearch")
 }
